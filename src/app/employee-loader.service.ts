0f9d308bfb64e6bbef44694cc5469ad57dea26af
ChrisHardin <chris.hardin@oasisdigital.com>
2019-01-24 13:19:41 -0600

    curriculum update

--- src/app/employee-loader.service.ts
+++ src/app/employee-loader.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const API_URL = 'https://api.angularbootcamp.com';
 
@@ -17,6 +18,8 @@ export class EmployeeLoaderService {
   constructor(private http: HttpClient) { }
 
   loadEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(API_URL + '/employees');
+    return this.http
+      .get<Employee[]>(API_URL + '/employees')
+      .pipe(map(employees => employees.slice(0, 5)));
   }
 }
